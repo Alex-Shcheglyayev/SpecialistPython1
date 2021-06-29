47079a436bbc45594eaecf67ffe5e3db35889947
Alex-Shcheglyayev <86655360+Alex-Shcheglyayev@users.noreply.github.com>
2021-06-29 12:37:35 +0300

    Update 02_task_if_else.py

--- Module2/practice/if_else/02_task_if_else.py
+++ Module2/practice/if_else/02_task_if_else.py
@@ -1,5 +1,5 @@
-# Дано целое число. Определить, заканчивается ли оно число цифрой 5?
-# Формат входных данных: Целое положительно число
-# Формат выходных данных: Если число оканчивается цифрой 5, вывести «YES», в противном случае — вывести «NO».
-
-# TODO: your code here
+number = int(input("Enter your number: "))
+if number % 10 == 5:
+    print("Your number ends with a 5")
+else:
+    print("Your number does not end with a 5")
